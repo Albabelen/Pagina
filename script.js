4ba533c42d438f06a9d4d08ae6462eb4bbc8aaa3
Albabelen <155817099+Albabelen@users.noreply.github.com>
2024-01-07 05:27:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function moveNo() {
         alert("¿Entonces me vas a clavar una duda? :(");
     }, 5000);
 }
+function moveNo() {
+    var noButton = document.getElementById('no');
+    noButton.style.left = Math.random() * 95 + "vw";
+    noButton.style.top = Math.random() * 95 + "vh";
+
+    createHeart();
+
+    createHeartRain();  // Agrega esta línea para iniciar la lluvia de corazones
+
+    setTimeout(function () {
+        alert("¿Entonces me vas a clavar una duda? :(");
+    }, 5000);
+}
+function createHeartRain() {
+    for (var i = 0; i < 10; i++) {
+        setTimeout(function () {
+            createHeart();
+        }, i * 300);  // Ajusta el intervalo de creación de corazones según tus preferencias
+    }
+}
