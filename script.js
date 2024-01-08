eceb9e9f595df7573d620a223d7ccbbfa6d65b5e
Albabelen <155817099+Albabelen@users.noreply.github.com>
2024-01-08 06:03:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,4 +41,13 @@ createStarRain();
 // Añade un evento para detectar cambios de orientación o tamaño de la ventana
 window.addEventListener('orientationchange', moveNoOnOrientationChange);
 window.addEventListener('resize', moveNoOnOrientationChange);
+// ... (código anterior)
 
+function showPoemAlert() {
+    alert("Antes de poner 'Sí' o 'No', me gustaría que leas este poema que te escribí en noviembre.");
+    setTimeout(function() {
+        alert("Entendí sobre destino,\ncuando apareciste días después de mi cumpleaños,\nenseñándome que amor no eran 4 letras\n y mucho menos Roma al revés.\n\nAmor eran tus manos\n sosteniendo mi café\n la primera vez que te vi.\n\nAmor era cada cosa que hacías;\nY créeme, estuve equivocada,\njamás me había enamorado,\nhasta ese martes.\n\nLos viernes se convirtieron en\nmi perfecta definición de amor,\nCuando agarrabas mi mano\n y mirábamos el atardecer.\n\nAmor era el brillo en tus ojos\n cuando hablas sobre lo que ama(ba)s\ny me incluias,\nAmor: eran tres letras, tu nombre.");
+    }, 500);
+}
+
+// ... (código posterior)
